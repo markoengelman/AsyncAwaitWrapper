e05295f82b18a5aae8270669eba878198ca7acf4
Marko Engelman <engelman.marko@gmail.com>
2021-10-30 09:49:43 +0200

    Initial Commit

---
+++ AsyncAwait/AsyncAwait.h
@@ -0,0 +1,18 @@
+//
+//  AsyncAwait.h
+//  AsyncAwait
+//
+//  Created by Marko Engelman on 30/10/2021.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for AsyncAwait.
+FOUNDATION_EXPORT double AsyncAwaitVersionNumber;
+
+//! Project version string for AsyncAwait.
+FOUNDATION_EXPORT const unsigned char AsyncAwaitVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <AsyncAwait/PublicHeader.h>
+
+
---
+++ AsyncAwaitTests/AsyncAwaitTests.swift
@@ -0,0 +1,13 @@
+//
+//  AsyncAwaitTests.swift
+//  AsyncAwaitTests
+//
+//  Created by Marko Engelman on 30/10/2021.
+//
+
+import XCTest
+@testable import AsyncAwait
+
+class AsyncAwaitTests: XCTestCase {
+
+}
