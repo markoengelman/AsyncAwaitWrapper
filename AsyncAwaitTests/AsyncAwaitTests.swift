4138228f1c609db4ddc222d643cdce9f46c7d840
Marko Engelman <engelman.marko@gmail.com>
2022-01-21 20:56:42 +0100

    Extract duplicate code into shared 'expect' helper method.

--- AsyncAwaitTests/AsyncAwaitTests.swift
+++ AsyncAwaitTests/AsyncAwaitTests.swift
@@ -12,49 +12,44 @@ class AsyncAwaitTests: XCTestCase {
     func test_wrapper_async_load_deliversResultEventually() throws {
         let (sut, _) = makeSUT()
         let exp = expectation(description: "Waiting AsyncAwaitWrapper to deliver")
-        Task.init {
+        expect(exp, when: {
             _ = try await sut.load().get()
-            exp.fulfill()
-        }
-        wait(for: [exp], timeout: 1.0)
+        })
     }
-    
+
     func test_wrapper_async_load_deliversResourceEventually() throws {
         let (sut, _) = makeSUT()
         let exp = expectation(description: "Waiting AsyncAwaitWrapper to deliver")
-        Task.init {
+        expect(exp, when: {
             let _: [String] = try await sut.load()
-            exp.fulfill()
-        }
-        wait(for: [exp], timeout: 1.0)
+        })
     }
     
     func test_wrapper_async_load_throwsError_onLoaderError() throws {
         let (sut, loader) = makeSUT()
         loader.complete(with: .failure(NSError(domain: "", code: -1, userInfo: nil)))
         let exp = expectation(description: "Waiting AsyncAwaitWrapper to throw")
-        Task.init {
+        expect(exp, when: {
+            var capturedError: Error?
             do {
                 let _: [String] = try await sut.load()
             } catch {
-                exp.fulfill()
+                capturedError = error
+                XCTAssertNotNil(capturedError)
             }
-        }
-        wait(for: [exp], timeout: 1.0)
+        })
     }
     
     func test_wrappedLoader_deliversEventually() {
         let (sut, _) = makeSUT()
         let exp = expectation(description: "Waiting wrapped loader to deliver")
-        
         var names: [String]?
-        sut.load {
-            names = try? $0.get()
-            exp.fulfill()
-        }
-        
-        wait(for: [exp], timeout: 1.0)
-        XCTAssertNotNil(names)
+        expect(exp, when: {
+            sut.load {
+                names = try? $0.get()
+                XCTAssertNotNil(names)
+            }
+        })
     }
 }
 
@@ -63,5 +58,13 @@ private extension AsyncAwaitTests {
     func makeSUT() -> (AsyncAwaitWrapper<[String], Error>, LocalUsersNamesLoader) {
         let loader = LocalUsersNamesLoader()
         return (AsyncAwaitWrapper(loader: loader.load), loader)
+    }
+    
+    func expect(_ exp: XCTestExpectation, when action: @escaping () async throws -> Void) {
+        Task {
+            try await action()
+            exp.fulfill()
+        }
+        wait(for: [exp], timeout: 1.0)
     }
 }
